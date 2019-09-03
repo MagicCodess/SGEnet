f485985bad49c2c04ddf1f4d54dbd51fc44ffb42
Akshat <akshat41121995@gmail.com>
2019-09-03 16:08:43 +0530

    Stable release 0.1

--- setup.py
+++ setup.py
@@ -2,11 +2,11 @@ import setuptools
 with open("README.md", "r") as fh:
     long_description = fh.read()
 setuptools.setup(
-     name='spellygrammar',  
-     version='0.2',
+     name='spellygrammarc',  
+     version='0.1',
      author="Akshat Gupta",
      author_email="akshat41121995@gmail.com",
-     py_modules = ["spellygrammar"],
+     py_modules = ["spellygrammarc"],
      description="Python package for spelling and grammar correction",
      url="https://github.com/akshat4112/sgnet",
      package_dir={'':'src'},
